ead6682ce787c332e8f5aa95016705bcab0426fc
HeshamFawzy <46792902+HeshamFawzy@users.noreply.github.com>
2020-09-09 22:46:39 +0200

    seeding

--- ecommerce/database/seeds/DatabaseSeeder.php
+++ ecommerce/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Admin;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        //create superAdmin
+        $superAdmin = Admin::create([
+            'name' => 'Admin',
+            'email' => 'Admin@test.com',
+            'password' => Hash::make('123456789')
+        ]);
 
-
+        $superAdminRole = Role::create(['guard_name' => 'admin', 'name' => 'superAdmin']);
+        $superAdmin->assignRole($superAdminRole);
     }
 }
