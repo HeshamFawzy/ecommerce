bcd7bed78fbe516b8f913abb7b8e105edaad85d8
HeshamFawzy <46792902+HeshamFawzy@users.noreply.github.com>
2020-09-10 14:51:40 +0200

    category api image

--- ecommerce/app/Http/Resources/CategoryResource.php
+++ ecommerce/app/Http/Resources/CategoryResource.php
@@ -9,15 +9,27 @@ class CategoryResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
+        if ($this->image_filename != null) {
+            $image = asset('categories/images/' . $this->image_filename);
+        } else {
+            $image = asset('assets/admin/images/Question_Mark.png');
+        }
+        if ($this->size_filename != null) {
+            $sizeImage = asset('categories/sizeImages/' . $this->size_filename);
+        } else {
+            $sizeImage = asset('assets/admin/images/Question_Mark.png');
+        }
         return [
             'id' => $this->id,
             'name_en' => $this->name_en,
             'name_ar' => $this->name_ar,
+            'image' => $image,
+            'size' => $sizeImage,
         ];
     }
 }
