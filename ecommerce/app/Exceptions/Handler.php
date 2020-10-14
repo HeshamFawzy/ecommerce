2a37357ea9425f97f465d4c2b5763cec9573afd5
HeshamFawzy <46792902+HeshamFawzy@users.noreply.github.com>
2020-10-14 13:06:08 +0200

    error handel

--- ecommerce/app/Exceptions/Handler.php
+++ ecommerce/app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use App\Error;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -29,7 +30,7 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Throwable  $exception
+     * @param \Throwable $exception
      * @return void
      *
      * @throws \Exception
@@ -42,14 +43,20 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
+     * @param \Illuminate\Http\Request $request
+     * @param \Throwable $exception
      * @return \Symfony\Component\HttpFoundation\Response
      *
      * @throws \Throwable
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception) {
+            // log the error
+            Error::create([
+                'error' => $exception
+            ]);
+        }
         return parent::render($request, $exception);
     }
 }
